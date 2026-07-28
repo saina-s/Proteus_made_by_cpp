b521ec56c9ac724d7af78ce3ccc1d7cba54b3f00
Farnoosh <ch.farnoosh2004@gmail.com>
2026-07-29 02:52:58 +0330

    First commit (just trying)

---
+++ main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Proteus Project Started!" << std::endl;
+    return 0;
+}
---
+++ main.h
@@ -0,0 +1,14 @@
+//
+// Created by Asus on 7/29/2026.
+//
+
+#ifndef PROTEUS_MADE_BY_CPP_MAIN_H
+#define PROTEUS_MADE_BY_CPP_MAIN_H
+
+
+class main
+{
+};
+
+
+#endif //PROTEUS_MADE_BY_CPP_MAIN_H
